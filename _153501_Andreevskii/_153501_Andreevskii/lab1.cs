4f25a5391ea8eb16d020fbd480d334ec4c8724f1
ILUHABRATUHA <ilya.andreevskii2004@gmail.com>
2022-03-08 21:15:02 +0300

    Lab1

---
+++ _153501_Andreevskii/_153501_Andreevskii/lab1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _153501_Andreevskii
+{
+    class lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers to find the quotient of their division: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            double c = a / (double)b;
+
+            Console.WriteLine("\nResult is " + c);
+        }
+    }
+}
