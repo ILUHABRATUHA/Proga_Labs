06e0cdad8543043447e64a3fac65c8635527e521
ILUHABRATUHA <ilya.andreevskii2004@gmail.com>
2022-04-28 11:39:49 +0300

    Laba_8

---
+++ Laba_8/task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Airport Domodedovo = Airport.CreateAirport("Domodedovo");
+            Tariff tariff = new Tariff("Moscow", "London", "250");
+            SpecialTariff spc_tar = new SpecialTariff("Paris", "Vitebsk", "440", "30");
+
+            Domodedovo.Add(tariff);
+            Domodedovo.Add(spc_tar);
+
+            Domodedovo.Show(); 
+            Domodedovo.ShowSpecial();
+
+            int a = Domodedovo.MaxTariff();
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Laba_8/task_1Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using task_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void maxtest1()
+        {
+            Airport ex = new Airport("name");
+
+            Assert.AreEqual(0, ex.MaxTariff());
+        }
+
+        [TestMethod()]
+        public void maxTest2()
+        {
+            Airport ex = Airport.CreateAirport("Domodedovo");
+            Tariff tariff = new Tariff("Moscow", "London", "250");
+            SpecialTariff spc_tar = new SpecialTariff("Paris", "Vitebsk", "440", "30");
+            ex.Add(tariff);
+            ex.Add(spc_tar);
+
+            Assert.AreEqual(308, ex.MaxTariff());
+        }
+  
+    }
+}
