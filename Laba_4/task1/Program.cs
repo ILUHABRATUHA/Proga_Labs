bdfbda41806fd623367a19d18691d7a53102721b
ILUHABRATUHA <ilya.andreevskii2004@gmail.com>
2022-04-28 11:32:03 +0300

    Laba_4

---
+++ Laba_4/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ZHES zhes = ZHES.getInstance();
+            
+            Console.WriteLine("Enter information about ZHES: ");
+
+            Console.WriteLine("\nEnter your district: ");
+            zhes.District = Console.ReadLine();
+
+            Console.WriteLine("Enter number of your ZHES ");
+            zhes.Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter amount of citizens: ");
+            zhes.CitizensAmount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number of citizens who has already paid : ");
+            zhes.NumberOFPaid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter tariff: ");
+            zhes.Tariff.Price = Convert.ToInt32(Console.ReadLine());
+
+            int debt = zhes.calculateDebt();
+            Console.WriteLine("\nDebt is " + debt);
+           
+        }
+    }
+}
